3552043c9bed48340e57bedbe0f5b859a6bc7f08
Sabinon <135654440+Sabinon@users.noreply.github.com>
2023-06-15 12:28:24 -0600

    Actualizar main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,242 @@
 namespace SpriteKind {
     export const objeto = SpriteKind.create()
 }
-let mySprite = sprites.create(assets.image`Carta`, SpriteKind.objeto)
-animation.runImageAnimation(
-mySprite,
-assets.animation`Carta`,
-100,
-true
-)
+controller.anyButton.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (true) {
+        animation.runImageAnimation(
+        Fantasmon,
+        [img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . f f f f f . . . . . 
+            . . . . . f c c c c c f . . . . 
+            . . . . f c c 1 f 1 f 1 f . . . 
+            . . . . f c 1 1 f 1 f 1 f . . . 
+            . . . . f c 1 3 1 1 1 3 f . . . 
+            . . . . f c 1 1 1 1 1 1 f . . . 
+            . . . . f c 1 1 1 1 1 1 f . . . 
+            . . . . f c 1 1 1 1 1 1 f . . . 
+            . . . . f c c 1 1 1 1 1 f . . . 
+            . . . . f c c c 1 1 1 1 f . . . 
+            . . . . f c c c f c 1 1 f . . . 
+            . . . . . f c f . f c f . . . . 
+            . . . . . . f . . . f . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `,img`
+            . . . . . . f f f f f . . . . . 
+            . . . . . f c c c c c f . . . . 
+            . . . . f c c 1 f 1 f 1 f . . . 
+            . . . . f c 1 1 f 1 f 1 f . . . 
+            . . . . f c 1 3 1 1 1 3 f . . . 
+            . . . . f c 1 1 1 1 1 1 f . . . 
+            . . . . f c 1 1 1 1 1 1 f . . . 
+            . . . . f c 1 1 1 1 1 1 f . . . 
+            . . . . f c c 1 1 1 1 1 f . . . 
+            . . . . f c c c 1 1 1 1 f . . . 
+            . . . . f c c c f c 1 1 f . . . 
+            . . . . . f c f . f c f . . . . 
+            . . . . . . f . . . f . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `,img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . f f f f f . . . . . 
+            . . . . . f c c c c c f . . . . 
+            . . . . f c c 1 f 1 f 1 f . . . 
+            . . . . f c 1 1 f 1 f 1 f . . . 
+            . . . . f c 1 3 1 1 1 3 f . . . 
+            . . . . f c 1 1 1 1 1 1 f . . . 
+            . . . . f c 1 1 1 1 1 1 f . . . 
+            . . . . f c 1 1 1 1 1 1 f . . . 
+            . . . . f c 1 1 1 1 1 1 f . . . 
+            . . . . f c 1 1 1 1 1 1 1 f . . 
+            . . . f c c c c 1 1 1 f c f . . 
+            . . f . f c f f c f f . f c f . 
+            . . . . . f . . f . . . . f . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `],
+        500,
+        true
+        )
+    }
+})
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (true) {
+        animation.runImageAnimation(
+        Fantasmon,
+        [img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . f f f f f f . . . 
+            . . . . . . . f 1 1 1 1 1 f . . 
+            . . . . . . f 1 1 f 1 f 1 f . . 
+            . . . . . . f 1 1 f 1 f 1 f . . 
+            . . . . . f f 1 3 1 1 1 3 f . . 
+            . . . . . f 1 1 1 1 1 1 1 f . . 
+            . . . . f c 1 1 1 1 1 1 f f . . 
+            . . . . f c 1 1 1 1 1 1 f . . . 
+            . . . f c 1 1 1 1 1 1 1 f . . . 
+            . . . f c f c c f c 1 1 f . . . 
+            . . f . f c f f . f c f . . . . 
+            . . . . . f . . . f f . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `,img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . f f f f f f . . . 
+            . . . . . . . f 1 1 1 1 1 f . . 
+            . . . . . . f 1 1 f 1 f 1 f . . 
+            . . . . . . f 1 1 f 1 f 1 f . . 
+            . . . . . . f 1 3 1 1 1 3 f . . 
+            . . . . . f 1 1 1 1 1 1 1 f . . 
+            . . . . f c 1 1 1 1 1 1 f f . . 
+            . . . . f c 1 1 1 1 1 1 f . . . 
+            . . . f c c c 1 1 1 1 1 f . . . 
+            . . f c c f f c c 1 1 f . . . . 
+            . . f c f . f c f f c f . . . . 
+            . . . f . . . f . . f . . . . . 
+            . . f . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `,img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . f f f f f f . . . 
+            . . . . . . . f 1 1 1 1 1 f . . 
+            . . . . . . f 1 1 f 1 f 1 f . . 
+            . . . . . . f 1 1 f 1 f 1 f . . 
+            . . . . . f f 1 3 1 1 1 3 f . . 
+            . . . . f c 1 1 1 1 1 1 1 f . . 
+            . . . f c 1 1 1 1 1 1 1 f f . . 
+            . . f c 1 1 1 1 1 1 1 1 f . . . 
+            . f c 1 1 1 1 1 1 1 1 c f . . . 
+            . f c f f c f c 1 c c f . . . . 
+            . . f . . f . f c f f . . . . . 
+            . . . . f . . . f . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `,img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . f f f f f f . . . 
+            . . . . . . . f 1 1 1 1 1 f . . 
+            . . . . . . f 1 1 f 1 f 1 f . . 
+            . . . . . . f 1 1 f 1 f 1 f . . 
+            . . . . . f f 1 3 1 1 1 3 f . . 
+            . . . . . f 1 1 1 1 1 1 1 f . . 
+            . . . . f c 1 1 1 1 1 1 f f . . 
+            . . . . f c 1 1 1 1 1 1 f . . . 
+            . . . f c 1 1 1 1 1 1 1 f . . . 
+            . . . f c f c c f c 1 1 f . . . 
+            . . f . f c f f . f c f . . . . 
+            . . . . . f . . . f f . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `,img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . f f f f f f . . . 
+            . . . . . . . f 1 1 1 1 1 f . . 
+            . . . . . . f 1 1 f 1 f 1 f . . 
+            . . . . . . f 1 1 f 1 f 1 f . . 
+            . . . . . . f 1 3 1 1 1 3 f . . 
+            . . . . . f 1 1 1 1 1 1 1 f . . 
+            . . . . f c 1 1 1 1 1 1 f f . . 
+            . . . . f c 1 1 1 1 1 1 f . . . 
+            . . . f c c c 1 1 1 1 1 f . . . 
+            . . f c c f f c c 1 1 f . . . . 
+            . . f c f . f c f f c f . . . . 
+            . . . f . . . f . . f . . . . . 
+            . . f . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `,img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . f f f f f f . . . 
+            . . . . . . . f 1 1 1 1 1 f . . 
+            . . . . . . f 1 1 f 1 f 1 f . . 
+            . . . . . . f 1 1 f 1 f 1 f . . 
+            . . . . . f f 1 3 1 1 1 3 f . . 
+            . . . . f c 1 1 1 1 1 1 1 f . . 
+            . . . f c 1 1 1 1 1 1 1 f f . . 
+            . . f c 1 1 1 1 1 1 1 1 f . . . 
+            . f c 1 1 1 1 1 1 1 1 c f . . . 
+            . f c f f c f c 1 c c f . . . . 
+            . . f . . f . f c f f . . . . . 
+            . . . . f . . . f . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `,img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . f f f f f f . . . 
+            . . . . . . . f 1 1 1 1 1 f . . 
+            . . . . . . f 1 1 f 1 f 1 f . . 
+            . . . . . . f 1 1 f 1 f 1 f . . 
+            . . . . . . f 1 3 1 1 1 3 f . . 
+            . . . . . f 1 1 1 1 1 1 1 f . . 
+            . . . . f c 1 1 1 1 1 1 f f . . 
+            . . . . f c 1 1 1 1 1 1 f . . . 
+            . . . f c c c 1 1 1 1 1 f . . . 
+            . . f c c f f c c 1 1 f . . . . 
+            . . f c f . f c f f c f . . . . 
+            . . . f . . . f . . f . . . . . 
+            . . f . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `,img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . f f f f f f . . . 
+            . . . . . . . f 1 1 1 1 1 f . . 
+            . . . . . . f 1 1 f 1 f 1 f . . 
+            . . . . . . f 1 1 f 1 f 1 f . . 
+            . . . . . f f 1 3 1 1 1 3 f . . 
+            . . . . . f 1 1 1 1 1 1 1 f . . 
+            . . . . f c 1 1 1 1 1 1 f f . . 
+            . . . . f c 1 1 1 1 1 1 f . . . 
+            . . . f c 1 1 1 1 1 1 1 f . . . 
+            . . . f c f c c f c 1 1 f . . . 
+            . . f . f c f f . f c f . . . . 
+            . . . . . f . . . f f . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `],
+        500,
+        false
+        )
+    }
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (true) {
+        Fantasmon.vy = -350
+    }
+})
+let Fantasmon: Sprite = null
+scene.setBackgroundColor(3)
+Fantasmon = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . f f f f f . . . . . 
+    . . . . . f c c c c c f . . . . 
+    . . . . f c c 1 f 1 f 1 f . . . 
+    . . . . f c 1 1 f 1 f 1 f . . . 
+    . . . . f c 1 3 1 1 1 3 f . . . 
+    . . . . f c 1 1 1 1 1 1 f . . . 
+    . . . . f c 1 1 1 1 1 1 f . . . 
+    . . . . f c 1 1 1 1 1 1 f . . . 
+    . . . . f c c 1 1 1 1 1 f . . . 
+    . . . . f c c c 1 1 1 1 f . . . 
+    . . . . f c c c f c 1 1 f . . . 
+    . . . . . f c f . f c f . . . . 
+    . . . . . . f . . . f . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
+info.setScore(0)
+controller.moveSprite(Fantasmon)
+scene.cameraFollowSprite(Fantasmon)
+tiles.setCurrentTilemap(tilemap`nivel2`)
+game.onUpdate(function () {
+    Fantasmon.vy += 50
+})
